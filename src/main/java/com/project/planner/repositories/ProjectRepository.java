df297dd48ef410b2657d0387d2986f0bdd6e4499
11jolek11 <dabrowskiandrzej094@gmail.com>
2024-02-19 17:46:28 +0100

    add repo content

--- src/main/java/com/project/planner/repositories/ProjectRepository.java
+++ src/main/java/com/project/planner/repositories/ProjectRepository.java
@@ -1,7 +1,13 @@
 package com.project.planner.repositories;
 
 import com.project.planner.models.Project;
+import com.project.planner.models.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+import java.util.Set;
+
 public interface ProjectRepository extends CrudRepository<Project, Long> {
+    Optional<Project> findProjectByMaintainer(User maintainer);
+    Set<Project> findProjectsByTitle(String title);
 }
--- src/main/java/com/project/planner/repositories/UserRepository.java
+++ src/main/java/com/project/planner/repositories/UserRepository.java
@@ -1,7 +1,12 @@
 package com.project.planner.repositories;
 
+import com.project.planner.models.Project;
 import com.project.planner.models.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends CrudRepository<User, Long> {
+    Optional<User> findUserByEmail(String email);
+    Optional<User> findUserByProject(Project project);
 }
