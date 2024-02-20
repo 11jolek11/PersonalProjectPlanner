1b62ec941cb77a5167fbdee5302faebe8b5b96c0
11jolek11 <dabrowskiandrzej094@gmail.com>
2024-02-20 18:01:52 +0100

    enables config and add command line runner for uploader init

--- src/main/java/com/project/planner/PlannerApplication.java
+++ src/main/java/com/project/planner/PlannerApplication.java
@@ -1,13 +1,26 @@
 package com.project.planner;
 
+import com.project.planner.configs.StorageConfig;
+import com.project.planner.services.StorageService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageConfig.class)
 public class PlannerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PlannerApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.initialize();
+		};
+	}
 }
