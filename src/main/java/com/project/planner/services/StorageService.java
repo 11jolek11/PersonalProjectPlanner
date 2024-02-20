967504dd75d1933355d10bf0fca146a4cccb8ab0
11jolek11 <dabrowskiandrzej094@gmail.com>
2024-02-20 18:02:04 +0100

    Services for file upload
    * StorageService - interface for storage classes
    * LocalFileSystemService - class which manages files stored in local
      file system

--- src/main/java/com/project/planner/services/StorageService.java
+++ src/main/java/com/project/planner/services/StorageService.java
@@ -1,4 +1,18 @@
 package com.project.planner.services;
 
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
 public interface StorageService {
+    void initialize();
+    void store(MultipartFile multipartFile);
+    Stream<Path> loadAllFilesPath();
+    Path loadFileByName(String filename);
+    Resource loadAsResource(String filename);
+
+    void deleteAll();
+    void deleteFile(String filename);
 }
