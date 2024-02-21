06ee7fe1f9cc337480d8f924de038555d77c59fa
11jolek11 <dabrowskiandrzej094@gmail.com>
2024-02-21 21:45:01 +0100

    Adds not implemented method for file handling in Task Service

--- src/main/java/com/project/planner/services/TaskService.java
+++ src/main/java/com/project/planner/services/TaskService.java
@@ -1,51 +1,40 @@
 package com.project.planner.services;
 
 import com.project.planner.exceptions.EntityInstanceDoesNotExist;
-import com.project.planner.models.Project;
 import com.project.planner.models.Task;
 import com.project.planner.models.TaskStatus;
-import com.project.planner.repositories.ProjectRepository;
 import com.project.planner.repositories.TaskRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
-import java.util.Set;
-
 @Service
 public class TaskService {
     private final TaskRepository taskRepository;
-    private final ProjectRepository projectRepository;
 
-    public TaskService(TaskRepository taskRepository, ProjectRepository projectRepository) {
+    public TaskService(TaskRepository taskRepository) {
         this.taskRepository = taskRepository;
-        this.projectRepository = projectRepository;
     }
 
-    public Long updateTaskStatus(Long taskId, TaskStatus newTaskStatus) {
-        Task targetTask = this.taskRepository.findById(taskId).orElseThrow(() -> {
+    public Task getTask(Long taskId) {
+        return this.taskRepository.findById(taskId).orElseThrow(() -> {
             return new EntityInstanceDoesNotExist(HttpStatus.NOT_FOUND, "Task NOT FOUND");
         });
+    }
 
+    // TODO(11jolek11): Implement
+//    public Set<FileSystemResource> getTaskFiles(Long taskId) {
+//
+//    }
+    // TODO(11jolek11): Implement
+//    public Long setTaskFile(Long taskId, String filename) {
+//
+//    }
+
+
+
+    public Long updateTaskStatus(Long taskId, TaskStatus newTaskStatus) {
+        Task targetTask = this.getTask(taskId);
         targetTask.setTaskStatus(newTaskStatus);
         return this.taskRepository.save(targetTask).getId();
-    }
-
-    public Long attachTaskToProject(Task newTask, Project existingProject) {
-        Set<Task> taskSet = existingProject.getTasks();
-        taskSet.add(newTask);
-        existingProject.setTasks(taskSet);
-
-        return this.projectRepository.save(existingProject).getId();
-    }
-    public Long attachTaskToProject(Task newTask, Long existingProjectId) {
-        Project existingProject = this.projectRepository.findById(existingProjectId).orElseThrow(() -> {
-            return new EntityInstanceDoesNotExist(HttpStatus.NOT_FOUND, "Project NOT FOUND");
-        });
-
-        Set<Task> taskSet = existingProject.getTasks();
-        taskSet.add(newTask);
-        existingProject.setTasks(taskSet);
-
-        return this.projectRepository.save(existingProject).getId();
     }
 }
