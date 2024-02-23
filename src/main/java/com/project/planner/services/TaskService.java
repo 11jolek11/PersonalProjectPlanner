b2e188f3501838efea03aeab912016b88437d1ed
11jolek11 <dabrowskiandrzej094@gmail.com>
2024-02-23 17:23:43 +0100

    Task service refactor
    Task service refactor which aims to reduce coupling between layers

--- src/main/java/com/project/planner/services/TaskService.java
+++ src/main/java/com/project/planner/services/TaskService.java
@@ -15,10 +15,30 @@ public class TaskService {
         this.taskRepository = taskRepository;
     }
 
-    public Task getTask(Long taskId) {
+    public Task findTask(Long taskId) {
         return this.taskRepository.findById(taskId).orElseThrow(() -> {
             return new EntityInstanceDoesNotExist(HttpStatus.NOT_FOUND, "Task NOT FOUND");
         });
+    }
+
+    public Boolean exists(Long id) {
+        return this.taskRepository.existsById(id);
+    }
+
+    public Task createTask(Task newTask) {
+        return this.taskRepository.save(newTask);
+    }
+
+    public void deleteTask(Long id) {
+        this.taskRepository.deleteById(id);
+    }
+
+    public Task updateExistingTask(Long existingTaskId, Task task) {
+        if (this.exists(existingTaskId) && task.getId().equals(existingTaskId)) {
+            return this.taskRepository.save(task);
+        }
+
+        throw new EntityInstanceDoesNotExist(HttpStatus.NOT_FOUND, "Task NOT FOUND");
     }
 
     // TODO(11jolek11): Implement
@@ -30,11 +50,10 @@ public class TaskService {
 //
 //    }
 
-
-
-    public Long updateTaskStatus(Long taskId, TaskStatus newTaskStatus) {
-        Task targetTask = this.getTask(taskId);
+    public Task updateTaskStatus(Long taskId, TaskStatus newTaskStatus) {
+        // TODO(11jolek11): Can I simplify this procedure?
+        Task targetTask = this.findTask(taskId);
         targetTask.setTaskStatus(newTaskStatus);
-        return this.taskRepository.save(targetTask).getId();
+        return this.taskRepository.save(targetTask);
     }
 }
