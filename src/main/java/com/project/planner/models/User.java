c277a789990091f11db66a701bc3877bf7534cc6
11jolek11 <dabrowskiandrzej094@gmail.com>
2024-02-23 01:44:46 +0100

    Adds missing methods for User entity

--- src/main/java/com/project/planner/models/User.java
+++ src/main/java/com/project/planner/models/User.java
@@ -33,6 +33,34 @@ public class User implements UserDetails {
         this.password = password;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Project> getProjects() {
+        return projects;
+    }
+
+    public void setProjects(Set<Project> projects) {
+        this.projects = projects;
+    }
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return null;
