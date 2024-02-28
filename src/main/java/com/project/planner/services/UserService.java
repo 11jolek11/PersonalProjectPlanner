1e0e09b19b2f376a02469dd3d3dd6b7da5379131
11jolek11 <dabrowskiandrzej094@gmail.com>
2024-02-28 21:33:30 +0100

    Adds method for authentication & minor refactor

--- src/main/java/com/project/planner/services/UserService.java
+++ src/main/java/com/project/planner/services/UserService.java
@@ -1,6 +1,7 @@
 package com.project.planner.services;
 
 import com.project.planner.exceptions.EntityInstanceDoesNotExist;
+import com.project.planner.exceptions.UserAlreadyExistsException;
 import com.project.planner.models.User;
 import com.project.planner.repositories.UserRepository;
 import org.springframework.http.HttpStatus;
@@ -22,18 +23,32 @@ public class UserService {
     public Boolean checkIfUserExists(String email) {
         return this.userRepository.existsByEmail(email);
     }
+
+    public User getUser(String email) {
+
+        return this.userRepository.findUserByEmail(email).orElseThrow(
+                () -> new EntityInstanceDoesNotExist(HttpStatus.NOT_FOUND, "User NOT FOUND")
+        );
+    }
+
+    protected User registerNewUser(User newUser) throws UserAlreadyExistsException {
+        if (this.checkIfUserExists(newUser.getUsername())) {
+            // TODO(11jolek11): Find better way to handle this type of event
+            throw new UserAlreadyExistsException(HttpStatus.BAD_REQUEST, "User already exists, try to login");
+        } else {
+            System.out.println("Trying to register new User with { name: " + newUser.getUsername() + ", password: " + newUser.getPassword());
+            return this.userRepository.save(newUser);
+        }
+    }
+
     public Boolean checkIfUserHaveFullRights(Long id) {
-        User targetUser = this.userRepository.findById(id).orElseThrow(() -> {
-            return new EntityInstanceDoesNotExist(HttpStatus.NOT_FOUND, "User NOT FOUND");
-        });
+        User targetUser = this.userRepository.findById(id).orElseThrow(() -> new EntityInstanceDoesNotExist(HttpStatus.NOT_FOUND, "User NOT FOUND"));
 
         return targetUser.isEnabled() && targetUser.isAccountNonLocked();
     }
 
     public Boolean checkIfUserHaveFullRights(String email) {
-        User targetUser = this.userRepository.findUserByEmail(email).orElseThrow(() -> {
-            return new EntityInstanceDoesNotExist(HttpStatus.NOT_FOUND, "User NOT FOUND");
-        });
+        User targetUser = this.userRepository.findUserByEmail(email).orElseThrow(() -> new EntityInstanceDoesNotExist(HttpStatus.NOT_FOUND, "User NOT FOUND"));
 
         return targetUser.isEnabled() && targetUser.isAccountNonLocked();
     }
