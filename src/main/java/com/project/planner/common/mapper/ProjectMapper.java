c9b3940c786b2235386cf35016ce20f6c11b0ad4
11jolek11 <dabrowskiandrzej094@gmail.com>
2024-03-05 20:05:23 +0100

    Mappers for Project/Task DTO <-> Project/Task Entity translation

---
+++ src/main/java/com/project/planner/common/mapper/ProjectMapper.java
@@ -0,0 +1,23 @@
+package com.project.planner.common.mapper;
+
+import com.project.planner.common.requests.CreateFullProjectRequest;
+import com.project.planner.models.Project;
+import org.modelmapper.ModelMapper;
+
+public class ProjectMapper implements Mapper<Project, CreateFullProjectRequest> {
+    ModelMapper mapper;
+
+    public ProjectMapper(ModelMapper mapper) {
+        this.mapper = mapper;
+    }
+
+    @Override
+    public CreateFullProjectRequest mapTo(Project project) {
+        return this.mapper.map(project, CreateFullProjectRequest.class);
+    }
+
+    @Override
+    public Project mapFrom(CreateFullProjectRequest createFullProjectRequest) {
+        return this.mapper.map(createFullProjectRequest, Project.class);
+    }
+}
---
+++ src/main/java/com/project/planner/common/mapper/TaskMapper.java
@@ -0,0 +1,24 @@
+package com.project.planner.common.mapper;
+
+import com.project.planner.common.requests.CreateFullTaskRequest;
+import com.project.planner.models.Task;
+import org.modelmapper.ModelMapper;
+
+public class TaskMapper implements Mapper<Task, CreateFullTaskRequest>{
+
+    ModelMapper mapper;
+
+    public TaskMapper(ModelMapper mapper) {
+        this.mapper = mapper;
+    }
+
+    @Override
+    public CreateFullTaskRequest mapTo(Task task) {
+        return this.mapper.map(task, CreateFullTaskRequest.class);
+    }
+
+    @Override
+    public Task mapFrom(CreateFullTaskRequest createFullTaskRequest) {
+        return this.mapper.map(createFullTaskRequest, Task.class);
+    }
+}
