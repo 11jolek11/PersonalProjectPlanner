da5ed55deaa08b6d6c9eef5d62bbad22bb290415
11jolek11 <dabrowskiandrzej094@gmail.com>
2024-03-05 20:21:22 +0100

    HOTFIX commit: Missing Component stereotype in Mapper implementations

--- src/main/java/com/project/planner/common/mapper/ProjectMapper.java
+++ src/main/java/com/project/planner/common/mapper/ProjectMapper.java
@@ -3,7 +3,10 @@ package com.project.planner.common.mapper;
 import com.project.planner.common.requests.CreateFullProjectRequest;
 import com.project.planner.models.Project;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ProjectMapper implements Mapper<Project, CreateFullProjectRequest> {
     ModelMapper mapper;
 
