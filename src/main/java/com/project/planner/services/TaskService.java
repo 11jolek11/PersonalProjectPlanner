978729cbddbc40cdfa38f37131165135423292fe
11jolek11 <dabrowskiandrzej094@gmail.com>
2024-03-12 12:45:20 +0100

    comments

--- src/main/java/com/project/planner/services/TaskService.java
+++ src/main/java/com/project/planner/services/TaskService.java
@@ -1,18 +1,30 @@
 package com.project.planner.services;
 
+import com.project.planner.common.AuthenticationFacade;
 import com.project.planner.exceptions.EntityInstanceDoesNotExist;
+import com.project.planner.models.Project;
 import com.project.planner.models.Task;
 import com.project.planner.models.TaskStatus;
+import com.project.planner.models.User;
+import com.project.planner.repositories.ProjectRepository;
 import com.project.planner.repositories.TaskRepository;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.stream.Stream;
 
 @Service
 public class TaskService {
     private final TaskRepository taskRepository;
+    private final ProjectRepository projectRepository;
+    private final AuthenticationFacade authentication;
 
-    public TaskService(TaskRepository taskRepository) {
+    public TaskService(TaskRepository taskRepository, ProjectRepository projectRepository, AuthenticationFacade authentication) {
         this.taskRepository = taskRepository;
+        this.projectRepository = projectRepository;
+        this.authentication = authentication;
     }
 
     public Task findTask(Long taskId) {
@@ -20,6 +32,24 @@ public class TaskService {
             return new EntityInstanceDoesNotExist(HttpStatus.NOT_FOUND, "Task NOT FOUND");
         });
     }
+
+    public Set<Task> findTask(Project originProject) {
+        return this.taskRepository.findTasksByOriginProject(originProject);
+    }
+
+//    public Task findTaskByOwner(Long taskId) {
+            // TODO(11jolek11): Write u own query
+//        User targetUser = (User) authentication.getAuthentication();
+//        Set<Project> projectByMaintainer = this.projectRepository.findProjectByMaintainer(targetUser);
+//        for (Project project: projectByMaintainer) {
+//            if project.getTasks()
+//        }
+//        return null;
+//    }
+
+//    public Set<Task> findTaskByUserId(Long userId) {
+//
+//    }
 
     public Boolean exists(Long id) {
         return this.taskRepository.existsById(id);
