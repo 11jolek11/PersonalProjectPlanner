e3bff06ce02c68a7c31b607db3de9e344e09ccb1
11jolek11 <dabrowskiandrzej094@gmail.com>
2024-03-12 12:49:28 +0100

    auth facade

---
+++ src/main/java/com/project/planner/common/IAuthenticationFacade.java
@@ -0,0 +1,9 @@
+package com.project.planner.common;
+
+
+import org.springframework.security.core.Authentication;
+
+public interface IAuthenticationFacade {
+    public Authentication getAuthentication();
+}
+
