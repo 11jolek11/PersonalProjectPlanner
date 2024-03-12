b4b7ec77bf7ad65ac2e7048837c99d55408b25b6
11jolek11 <dabrowskiandrzej094@gmail.com>
2024-03-12 12:27:57 +0100

    change in repositories - adaptation for authorization

--- src/main/java/com/project/planner/repositories/TaskRepository.java
+++ src/main/java/com/project/planner/repositories/TaskRepository.java
@@ -4,6 +4,7 @@ import com.project.planner.models.Project;
 import com.project.planner.models.Task;
 import com.project.planner.models.TaskStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -12,8 +13,18 @@ import java.util.Set;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
     Set<Task> findAllByTitle(String title);
+//    Set<Task> findAllByOriginProject(Project originProject);
     List<Task> findAllByCreatedDateBefore(LocalDate createdDate);
     List<Task> findAllByCreatedDateAfter(LocalDate createdDate);
     List<Task> findAllByTaskStatus(TaskStatus taskStatus);
     Set<Task> findTasksByOriginProject(Project originProject);
+
+    String query = "SELECT task.created_date, task.deadline, task.id, task.origin_project, task.description, task.task_status, task.title, task.notes"
+            + " FROM"
+            + " project JOIN task ON task.origin_project = project.id"
+            + " JOIN _user ON project.maintainer_id = _user.id"
+            + " WHERE _user.id=?1";
+    @Query(value = query, nativeQuery=true)
+    Set<Task> findTasksByMaintainerId(Long maintainerId);
+
 }
