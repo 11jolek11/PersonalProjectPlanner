a766a107038873c520d0585341738314402280c4
11jolek11 <dabrowskiandrzej094@gmail.com>
2024-07-17 02:47:38 +0200

    Auth changes

--- src/main/java/com/project/planner/common/AuthenticationFacade.java
+++ src/main/java/com/project/planner/common/AuthenticationFacade.java
@@ -4,6 +4,7 @@ package com.project.planner.common;
 import org.springframework.security.core.Authentication;
 
 public interface AuthenticationFacade {
+    // TODO(11jolek11): Add getEmail or getName method?
     public Authentication getAuthentication();
 }
 
