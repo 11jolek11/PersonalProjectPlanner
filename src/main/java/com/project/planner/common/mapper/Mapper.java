7bd99fcf8c5245b90f63caf5657704ccb4def1d3
11jolek11 <dabrowskiandrzej094@gmail.com>
2024-07-22 10:30:23 +0200

    Mapper update

--- src/main/java/com/project/planner/common/mapper/Mapper.java
+++ src/main/java/com/project/planner/common/mapper/Mapper.java
@@ -3,4 +3,8 @@ package com.project.planner.common.mapper;
 public interface Mapper<A, B> {
     B mapTo(A a);
     A mapFrom(B b);
+    // AA
+    // BA
+//    void update(B b, A a);
+
 }
--- src/main/java/com/project/planner/common/mapper/TaskMapper.java
+++ src/main/java/com/project/planner/common/mapper/TaskMapper.java
@@ -1,13 +1,19 @@
 package com.project.planner.common.mapper;
 
+import com.project.planner.common.dto.ProjectDTO;
 import com.project.planner.common.dto.TaskDTO;
+import com.project.planner.models.Project;
 import com.project.planner.models.Task;
+import com.project.planner.models.User;
+import org.modelmapper.Conditions;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 @Component
 public class TaskMapper implements Mapper<Task, TaskDTO>{
-
     ModelMapper mapper;
 
     public TaskMapper(ModelMapper mapper) {
@@ -23,5 +29,11 @@ public class TaskMapper implements Mapper<Task, TaskDTO>{
     @Override
     public Task mapFrom(TaskDTO createFullTaskRequest) {
         return this.mapper.map(createFullTaskRequest, Task.class);
+    }
+
+    public Task update(TaskDTO dto, Task task) {
+        mapper.getConfiguration().setPropertyCondition(Conditions.isNotNull());
+        this.mapper.map(dto, task);
+        return task;
     }
 }
